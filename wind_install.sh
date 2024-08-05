8e9e5f15b05b3b0ec21d3ab884951121d492098a
capt-dev <jihjhuhjjjj@gmail.com>
2024-08-05 16:20:18 +0530

    Add files via upload

---
+++ wind_install.sh
@@ -0,0 +1,16 @@
+echo "installing necessary packages..."
+pkg update && pkg upgrade
+pkg install -y x11-repo && pkg install -y termux-x11-nightly openbox xfce4-panel xcompmgr pcmanfm nitrogen cairo-dock-core wget zip unzip gedit firefox
+echo "getting required configuration folder...(warning this will destroy all configurations)"
+unzip .config.zip
+rm .config.zip
+rm -rf ~/.config
+mv .config ~/
+echo "getting the folders for all the icons"
+unzip .icons.zip
+rm .icons.zip
+mv .icons ~/
+echo "putting the needed script to start wind in the correct location..."
+mv wind /data/data/com.termux/files/usr/bin
+chmod +x /data/data/com.termux/files/usr/bin/wind
+echo "the environment has successfully been installed please type ""wind"" to start it"
